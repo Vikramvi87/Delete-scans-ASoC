3c6c2468bd9076135f774a33c1f0e8dda1aecbe8
jrocia <69405400+jrocia@users.noreply.github.com>
2022-08-08 12:41:03 -0300

    Update deleteScans.sh

--- deleteScans.sh
+++ deleteScans.sh
@@ -14,7 +14,7 @@ ASOCkeySecret='xxxxxxxxxxxxxxxxxxxxxxxxxxx'
 startDate=$1
 endDate=$2
 
-ASOCtoken=$(curl -s -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"KeyId":"'"${ASOCkeyId}"'","KeySecret":"'"${ASOCkeySecret}"'"}' 'https://cloud.appscan.com/api/V2/Account/ApiKeyLogin' | grep -oP '(?<="Token":")[^"]*')
+ASOCtoken=$(curl -s -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"KeyId":"'"${ASOCkeyId}"'","KeySecret":"'"${ASOCkeySecret}"'"}' 'https://cloud.appscan.com/api/V2/Account/ApiKeyLogin' | jq -r .Token)
 
 scanIdCount=$(curl --header 'Content-Type: application/json' --header 'Accept: application/json' --header "Authorization: Bearer $ASOCtoken"  'https://cloud.appscan.com/api/v2/Scans/GetAsPage?&$filter=((Technology%20eq%20%27StaticAnalyzer%27))and(LatestExecution/ExecutedAt%20gt%20datetime%27'"$startDate"'T03:00:00.000Z%27%20and%20LatestExecution/ExecutedAt%20lt%20datetime%27'"$endDate"'T02:59:59.000Z%27)&$inlinecount=allPages' | jq -r .Items[].Id | wc -l)
 
@@ -23,7 +23,7 @@ scanIdCountArray="$((scanIdCount-1))"
 scanIds=($(curl --header 'Content-Type: application/json' --header 'Accept: application/json' --header "Authorization: Bearer $ASOCtoken" 'https://cloud.appscan.com/api/v2/Scans/GetAsPage?&$filter=((Technology%20eq%20%27StaticAnalyzer%27))and(LatestExecution/ExecutedAt%20gt%20datetime%27'"$startDate"'T03:00:00.000Z%27%20and%20LatestExecution/ExecutedAt%20lt%20datetime%27'"$endDate"'T02:59:59.000Z%27)&$inlinecount=allPages' | jq -r .Items[].Id))
 
 for i in ${!scanIds[@]}
-	do 
-		echo "element $i is ${scanIds[$i]}";  
-		curl -X 'DELETE' --header 'Content-Type: application/json' --header 'Accept: application/json' --header "Authorization: Bearer $ASOCtoken"  "https://cloud.appscan.com/api/V2/Scans/${scanIds[$i]}?deleteIssues=false"
-	done
+  do 
+    echo "Element $i, scanid ${scanIds[$i]} deleted";  
+    curl -X 'DELETE' --header 'Content-Type: application/json' --header 'Accept: application/json' --header "Authorization: Bearer $ASOCtoken"  "https://cloud.appscan.com/api/V2/Scans/${scanIds[$i]}?deleteIssues=false"
+  done
